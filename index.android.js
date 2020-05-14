33f6c9838fb06ec8c3d5e785823b8474f6d1981c
MAC <ahmadchand420@gmail.com>
2020-05-14 17:32:01 +0500

    First commit after watching tutorial

---
+++ index.android.js
@@ -0,0 +1,23 @@
+/**
+ * @format
+ */
+
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+
+// AppRegistry.registerComponent(appName, () => App);
+
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+const App = () => (
+    <View style={{ flex: 1 }}>
+        <Header headerText={'Albums'} />
+        <AlbumList />
+    </View>
+);
+
+AppRegistry.registerComponent('albums', () => App);
---
+++ src/components/Card.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const Card = (props) => {
+    return (
+        <View style={styles.containerStyle}>
+            {props.children}
+        </View>
+    );
+};
+
+const styles = {
+    containerStyle: {
+
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+
+        ShadowColor: '#000',
+        ShadowOffset: { width: 0, height: 2 },
+        ShadowOpasity: 0.1,
+        ShadowRadius: 2,
+
+        elevation: 1,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10
+    }
+};
+
+export default Card;
